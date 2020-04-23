76d9e66bbe4ca02ddee23be18d45990729fd0c2f
laci <216ladislavhric@gmail.com>
2020-04-24 01:06:16 +0200

    sass added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './homepage.component.jsx';
+import Bloody from './bloody.component'
 
 function App() {
   return (
     <div className="App">
+      <Bloody />
+      {/* <h1> Helllo Function Components </h1> */}
       <HomePage />
-      <h1> Helllo no</h1>
+      
+     
     </div>
   );
 }
---
+++ src/bloody.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.scss';
+
+
+const bloody = () => {
+  return (
+    <div>
+        <h1> RAFCE ... updated  </h1>
+    </div>
+  );
+}
+
+export default bloody;
