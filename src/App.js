a1a569007e7f47db8bfd9b8da168ebc7ae3d09ab
laci <216ladislavhric@gmail.com>
2020-04-23 23:35:58 +0200

    first react commmit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      <h1> Helllo no</h1>
+    </div>
+  );
+}
+
+export default App;
