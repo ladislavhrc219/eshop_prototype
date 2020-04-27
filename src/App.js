75e275da525c259a58f6e379593d8722f6c8bafd
laci <216ladislavhric@gmail.com>
2020-04-27 16:53:36 +0200

    header logo navigation added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
+
 import {Switch, Route} from 'react-router-dom';
 
 // import CollectionPreview from './components/collection-preview/collection-preview.jsx';
 
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import HomePage from './pages/homepage/homepage.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div className="App">
-
-    <Switch>
-
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-
+      <Header />
+      <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+      </Switch> 
       
-    </Switch> 
-      
-   
-      {/* <HomePage /> */}
-      
-     
     </div>
   );
 }
